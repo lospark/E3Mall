8a544f16e253c94a33e0fdf0869588c86090e1c9
lospark <786834592@qq.com>
2017-12-19 21:37:30 +0800

    Changes to be committed: 	modified:   e3-item-web/pom.xml 	new file:   e3-item-web/src/main/webapp/WEB-INF/ftl/hello.ftl 	new file:   e3-item-web/src/test/java/cn/lospark/freemarker/FreeMarkerTest.java 	modified:   e3-manager/e3-manager-service/src/main/java/cn/lospark/service/impl/ItemServiceImpl.java 	new file:   e3-manager/e3-manager-service/src/main/resources/conf/resource.properties 	modified:   e3-manager/e3-manager-service/src/main/resources/spring/applicationContext-dao.xml

--- e3-manager/e3-manager-service/src/main/java/cn/lospark/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/cn/lospark/service/impl/ItemServiceImpl.java
@@ -9,15 +9,19 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.druid.support.json.JSONUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.lospark.common.pojo.EasyUIDataGridResult;
+import cn.lospark.jedis.JedisClient;
 import cn.lospark.mapper.TbItemDescMapper;
 import cn.lospark.mapper.TbItemMapper;
 import cn.lospark.pojo.TbItem;
@@ -27,6 +31,7 @@ import cn.lospark.pojo.TbItemExample.Criteria;
 import cn.lospark.service.ItemService;
 import cn.lospark.utils.E3Result;
 import cn.lospark.utils.IDUtils;
+import cn.lospark.utils.JsonUtils;
 
 /**
  * 商品管理service
@@ -44,13 +49,32 @@ public class ItemServiceImpl implements ItemService {
 	private JmsTemplate jmsTemplate;
 	@Resource
 	private Destination topicDestination;
+	@Autowired
+	private JedisClient jedisClient;
 	
+	@Value("${REDIS_ITEM_PRE}")
+	private String REDIS_ITEM_PRE;
+	
+	@Value("${ITEM_CACHE_EXPIRE}")
+	private Integer ITEM_CACHE_EXPIRE;
 	
 	@Override
 	public TbItem getItemById(long itemId) {
+		//查询缓存
+		try {
+			String json = jedisClient.get(REDIS_ITEM_PRE+":"+itemId+":BASE");
+			if (StringUtils.isNoneBlank(json)) {
+				TbItem item = JsonUtils.jsonToPojo(json, TbItem.class);
+				return item;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//缓存中没有查询数据库
+		
 		//根据主键查询
 		//TbItem selectByPrimaryKey = itemMapper.selectByPrimaryKey(itemId);
-		
 		//设置条件进行查询。
 		//1.创建查询条件对象。
 		TbItemExample example =  new TbItemExample();
@@ -60,6 +84,15 @@ public class ItemServiceImpl implements ItemService {
 		//3.执行查询
 		 List<TbItem> list = itemMapper.selectByExample(example);
 		 if (null != list && list.size() > 0) {
+			 //把结果添加到缓存
+			try {
+				//添加缓存
+				 jedisClient.set(REDIS_ITEM_PRE+":"+itemId+":BASE", JsonUtils.objectToJson(list.get(0)));
+				 //设置过期时间
+				 jedisClient.expire(REDIS_ITEM_PRE+":"+itemId+":BASE", ITEM_CACHE_EXPIRE);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 			return list.get(0);
 		}
 		return null;
@@ -115,7 +148,28 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public TbItemDesc getItemDescById(long itemId) {
+		//查询缓存
+		try {
+			String json = jedisClient.get(REDIS_ITEM_PRE+":"+itemId+":DESC");
+			if (StringUtils.isNoneBlank(json)) {
+				TbItemDesc ibItemDesc = JsonUtils.jsonToPojo(json, TbItemDesc.class);
+				return ibItemDesc;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//缓存中没有查询数据库
 		TbItemDesc tbItemDesc = descMapper.selectByPrimaryKey(itemId);
+		 //把结果添加到缓存
+		try {
+			//添加缓存
+			 jedisClient.set(REDIS_ITEM_PRE+":"+itemId+":DESC", JsonUtils.objectToJson(tbItemDesc));
+			 //设置过期时间
+			 jedisClient.expire(REDIS_ITEM_PRE+":"+itemId+":DESC", ITEM_CACHE_EXPIRE);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return tbItemDesc;
 	}
 
